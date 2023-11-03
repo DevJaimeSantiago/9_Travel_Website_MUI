957bd9ee3125f22bdb7ddfd86433e72de914bf9d
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-11-03 12:53:13 -0600

    feat: Componente Navbar Finalizado

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import { navItems } from "../../data.js";
 
 // Components - Material UI
-import { AppBar, Box, Container, Link, Typography } from "@mui/material";
+import {
+	AppBar,
+	Box,
+	Container,
+	Drawer,
+	Link,
+	Typography,
+} from "@mui/material";
 
 // Material icons - Material UI
 import MenuIcon from "@mui/icons-material/Menu";
 
 // images
 import logo from "../../assets/main/logo.svg";
+import UserAvatar from "../../assets/main/user-avatar.svg";
 
 const Navbar = () => {
+	const [openMobileMenu, setOpenMobileMenu] = useState(false);
+
 	return (
 		<AppBar
 			position="static"
@@ -87,8 +98,141 @@ const Navbar = () => {
 						>
 							Holla.
 						</Typography>
+						<Typography
+							sx={{
+								fontWeight: "600",
+								fontSize: "14px",
+								lineHeight: "21px",
+								color: "#FFF",
+							}}
+						>
+							Jaime Santiago
+						</Typography>
 					</Box>
+					<img
+						src={UserAvatar}
+						alt="avatar-user"
+						style={{
+							cursor: "pointer",
+						}}
+					/>
 				</Box>
+				<MenuIcon
+					sx={{
+						display: {
+							xs: "block",
+							lg: "none",
+						},
+						cursor: "pointer",
+						color: "#FFF",
+					}}
+					onClick={() => setOpenMobileMenu(true)}
+				/>
+				<Drawer
+					anchor="left"
+					open={openMobileMenu}
+					onClose={() => setOpenMobileMenu(false)}
+				>
+					<Box
+						sx={{
+							position: "relative",
+							width: 250,
+							backgroundColor: "#5243C2",
+							height: "100%",
+							py: 3,
+							px: 1,
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+							gap: 5,
+						}}
+						role="presentation"
+						onClick={() => setOpenMobileMenu(false)}
+						onKeyDown={() => setOpenMobileMenu(false)}
+					>
+						<Box
+							sx={{
+								display: "flex",
+								alignItems: "center",
+								gap: "8px",
+							}}
+						>
+							<Box>
+								<Typography
+									sx={{
+										fontWeight: "400",
+										fontSize: "14px",
+										lineHeight: "21px",
+										color: "#FFF",
+										opacity: "0.5",
+									}}
+								>
+									Holla.
+								</Typography>
+								<Typography
+									sx={{
+										fontWeight: "600",
+										fontSize: "14px",
+										lineHeight: "21px",
+										color: "#FFF",
+									}}
+								>
+									Jaime Santiago
+								</Typography>
+							</Box>
+							<img
+								src={UserAvatar}
+								alt="avatar-user"
+								style={{
+									cursor: "pointer",
+								}}
+							/>
+						</Box>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "column",
+								alignItems: "center",
+								gap: 3,
+							}}
+						>
+							{navItems.map((navItem) => (
+								<Link
+									href="#"
+									key={navItem.id}
+									underline="none"
+									color="#FFF"
+									sx={{
+										fontWeight: "300",
+										fontSize: "14px",
+										opacity: 0.7,
+										"&:hover": {
+											opacity: 1,
+										},
+										"&:first-of-type": {
+											opacity: 1,
+										},
+									}}
+								>
+									{navItem.name}
+								</Link>
+							))}
+						</Box>
+						<Typography
+							sx={{
+								color: "#FFF",
+								fontWeight: "500",
+								cursor: "pointer",
+								position: "absolute",
+								bottom: "14px",
+								left: "50%",
+								transform: "translateX(-50%)",
+							}}
+						>
+							© 2rism
+						</Typography>
+					</Box>
+				</Drawer>
 			</Container>
 		</AppBar>
 	);
