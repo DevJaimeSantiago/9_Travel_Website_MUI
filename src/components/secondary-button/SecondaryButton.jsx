2a04f22dc0d679047b263cf159e72e5117a41c36
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-11-06 12:53:23 -0600

    refactor: Creacion del Componente SecondaryButton

--- src/components/secondary-button/SecondaryButton.jsx
+++ src/components/secondary-button/SecondaryButton.jsx
@@ -1,6 +1,41 @@
-const SecondaryButton = () => {
-  return (
-    <div>SecondaryButton</div>
-  )
-}
-export default SecondaryButton
+// images - Icons
+import btnArrowIcon from "../../assets/main/btn-arrow-icon.svg";
+
+// Components - Material UI
+import { Button } from "@mui/material";
+
+const SecondaryButton = ({ text }) => {
+	return (
+		<Button
+			variant="contained"
+			sx={{
+				width: {
+					xs: "100%",
+					md: "auto",
+				},
+				backgroundColor: "#F6F4FF",
+				textTransform: "none",
+				p: 1.25,
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				gap: 0.5,
+				borderRadius: "12px",
+				fontFamily: "inherit",
+				fontSize: "16px",
+				fontWeight: "400",
+				color: "#5243C2",
+				"&.MuiButtonBase-root": {
+					backgroundColor: "#F6F4FF",
+				},
+			}}
+		>
+			{text}
+			<img
+				src={btnArrowIcon}
+				alt="btnArrowIcon"
+			/>
+		</Button>
+	);
+};
+export default SecondaryButton;
