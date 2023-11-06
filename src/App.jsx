0c5cc686e30ca259027def5cddfc6652106aa6fa
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-11-06 13:49:59 -0600

    feat: Agregado el componente Travel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Hero from "./components/hero/Hero";
 import Destinations from "./components/destinations/Destinations";
 import HotelAndRestaurants from "./components/hotels-and-restaurants/HotelAndRestaurants";
+import Travel from "./components/travel/Travel";
 
 const App = () => {
 	return (
@@ -9,6 +10,7 @@ const App = () => {
 			<Hero />
 			<Destinations />
 			<HotelAndRestaurants />
+			<Travel />
 		</>
 	);
 };
---
+++ src/components/travel-item/TravelItem.jsx
@@ -0,0 +1,11 @@
+const TravelItem = ({
+	image,
+	location,
+	text,
+	calendarText,
+	userText,
+	commentsText,
+}) => {
+	return <div>TravelItem</div>;
+};
+export default TravelItem;
