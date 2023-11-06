b988ebebd4830f475c3b02d19192824e6db77516
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-11-06 14:17:11 -0600

    feat: Agregado fuente Poppins

--- src/components/travel-item/TravelItem.jsx
+++ src/components/travel-item/TravelItem.jsx
@@ -1,3 +1,11 @@
+// Components - Material UI
+import { Box } from "@mui/material";
+
+// Images - Icons
+import calendarIcon from "../../assets/travel/calendar-icon.svg";
+import userIcon from "../../assets/travel/user-icon.svg";
+import commentsIcon from "../../assets/travel/comments-icon.svg";
+
 const TravelItem = ({
 	image,
 	location,
@@ -6,6 +14,30 @@ const TravelItem = ({
 	userText,
 	commentsText,
 }) => {
-	return <div>TravelItem</div>;
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				gap: 2,
+				flexDirection: {
+					xs: "column",
+					md: "row",
+				},
+			}}
+		>
+			<Box>
+				<img
+					src={image}
+					alt="image"
+					style={{
+						objectFit: "cover",
+						height: "100%",
+						maxWidth: "100%",
+						cursor: "pointer",
+					}}
+				/>
+			</Box>
+		</Box>
+	);
 };
 export default TravelItem;
