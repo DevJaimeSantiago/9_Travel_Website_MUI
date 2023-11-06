5039736733b44def1b2a8299aa96b020c14b48ec
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-11-06 12:25:25 -0600

    refactor: Creacion del componente HotelRestaurantItem

--- src/components/hotel-restaurant-item/HotelRestaurantItem.jsx
+++ src/components/hotel-restaurant-item/HotelRestaurantItem.jsx
@@ -1,6 +1,79 @@
-const HotelRestaurantItem = () => {
-  return (
-    <div>HotelRestaurantItem</div>
-  )
-}
-export default HotelRestaurantItem
+// Images
+import hotelsLocationIcon from "../../assets/hotels-restaurants/hotels-location-icon.svg";
+
+// Components - Material UI
+import { Box, Typography } from "@mui/material";
+
+const HotelRestaurantItem = ({ name, location, image, ratingImage }) => {
+	return (
+		<Box sx={{ display: "flex", flexDirection: "column" }}>
+			<Box
+				sx={{
+					width: {
+						xs: "70vw",
+						sm: "60vw",
+						md: "auto",
+					},
+					display: "flex",
+					justifyContent: "center",
+				}}
+			>
+				<img
+					src={image}
+					alt="imageHotels"
+					style={{ width: "100%", objectFit: "cover", cursor: "pointer" }}
+				/>
+			</Box>
+			<Typography
+				sx={{
+					mt: 1.25,
+					fontFamily: "Poppins",
+					fontWeight: "500",
+					fontSize: "16px",
+					color: "#161414",
+					maxWidth: "252px",
+				}}
+			>
+				{name}
+			</Typography>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: {
+						xs: "column",
+						lg: "row",
+					},
+					alignItems: {
+						xs: "flex-start",
+						lg: "center",
+					},
+					justifyContent: "space-between",
+					gap: 1,
+					mt: 1.5,
+				}}
+			>
+				<Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+					<img
+						src={hotelsLocationIcon}
+						alt="hotelsLocationIcon"
+					/>
+					<Typography
+						sx={{
+							fontFamily: "Poppins",
+							fontWeight: "400",
+							fontSize: "13px",
+							color: "#979797",
+						}}
+					>
+						{location}
+					</Typography>
+				</Box>
+				<img
+					src={ratingImage}
+					alt="ratingImage"
+				/>
+			</Box>
+		</Box>
+	);
+};
+export default HotelRestaurantItem;
--- src/components/hotels-and-restaurants/HotelAndRestaurants.jsx
+++ src/components/hotels-and-restaurants/HotelAndRestaurants.jsx
@@ -2,6 +2,7 @@ import { hotelsRestaurants } from "../../data";
 
 // Components
 import HotelRestaurantItem from "../hotel-restaurant-item/HotelRestaurantItem";
+import SecondaryButton from "../secondary-button/SecondaryButton";
 
 // Components - Material UI
 import { Box, Container, Typography } from "@mui/material";
@@ -29,7 +30,7 @@ const HotelAndRestaurants = () => {
 					},
 					alignItems: "center",
 					justifyContent: "space-between",
-					md: 8,
+					mb: 8,
 					gap: 2,
 				}}
 			>
@@ -47,24 +48,33 @@ const HotelAndRestaurants = () => {
 				>
 					Hotels and Restaurants
 				</Typography>
+				<SecondaryButton text="View All" />
 			</Box>
-            <Box sx={{
-                display: "flex",
-                gap: 4,
-                justifyContent: "space-between",
-                alignItems: {
-                    xs: "center",
-                    md: "flex-start",
-                },
-                flexDirection: {
-                    xs: "column",
-                    md: "row",
-                },
-            }}>
-                {hotelsRestaurants.map((item) =>(
-                    <HotelRestaurantItem />
-                ) )}
-            </Box>
+			<Box
+				sx={{
+					display: "flex",
+					gap: 4,
+					justifyContent: "space-between",
+					alignItems: {
+						xs: "center",
+						md: "flex-start",
+					},
+					flexDirection: {
+						xs: "column",
+						md: "row",
+					},
+				}}
+			>
+				{hotelsRestaurants.map((item) => (
+					<HotelRestaurantItem
+						key={item.id}
+						name={item.name}
+						location={item.location}
+						image={item.image}
+						ratingImage={item.ratingImage}
+					/>
+				))}
+			</Box>
 		</Container>
 	);
 };
